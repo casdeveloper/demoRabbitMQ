191d3279a5c879d134d25e2235f8de32b809f0e0
Carlos Alves <casdeveloper@icloud.com>
2025-01-30 11:11:58 -0300

    Refetoring project

--- dataserver/src/main/java/com/casdeveloper/dataserver/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.casdeveloper.dataserver.repository;
-
-import com.casdeveloper.dataserver.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
